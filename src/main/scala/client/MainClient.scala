9c91f4bb2f87b85b97d62d9ff79f3fabff934982
Artur Abdalimov <a.abdalimov.97@gmail.com>
2017-09-20 04:44:24 +0600

    Try Catch Block in server, more functional way of reading message

--- src/main/scala/client/MainClient.scala
+++ src/main/scala/client/MainClient.scala
@@ -10,15 +10,15 @@ import scala.io.StdIn._
   * Created by Artur on 19.09.2017.
   */
 object MainClient extends App {
+
   val socket = new Socket("localhost", 9090)
-
   val outputStream = new DataOutputStream(socket.getOutputStream)
-
   new Thread(new ClientThread(socket)).start()
 
   while(true) {
-    val line = readLine()
+    var line = readLine()
     println(s"Sending $line")
+    line += "\n"
     outputStream.write(line.getBytes(), 0, line.length)
     outputStream.flush()
   }
--- src/main/scala/server/ServerThread.scala
+++ src/main/scala/server/ServerThread.scala
@@ -1,29 +1,23 @@
 package server
 
 import java.io.{DataInputStream, DataOutputStream}
-import java.net.Socket
+import java.net.{Socket, SocketException}
 
 /**
   * Created by Artur on 19.09.2017.
   */
 class ServerThread(val socket: Socket) extends Runnable {
 
-  val inputStream = new DataInputStream(socket.getInputStream())
-  val outputStream = new DataOutputStream(socket.getOutputStream())
-
   override def run(): Unit = {
-    println("Running")
-    while(true) {
-      val buffer = new Array[Byte](1024)
-      val string = new StringBuilder()
-      println("Start reading from inputStream")
-      inputStream.read(buffer)
-      println(s"Message ${new String(buffer, "utf-8")}")
-//      Stream.continually(inputStream.read(buffer)).takeWhile(_ != -1).foreach(string.append(new String(buffer)))
-      println("Read String")
-      if (!string.isEmpty) {
-        println(string.toString())
+
+    try {
+      while (socket.isConnected) {
+        val received = new String(Stream.continually(socket.getInputStream.read).takeWhile(_ != '\n').map(_.toByte).toArray)
+        println(received)
       }
+    } catch {
+      case s: SocketException => println("Socket closed")
     }
+    println("End of session")
   }
 }
