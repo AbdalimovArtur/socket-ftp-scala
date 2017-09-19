cfdf59fad7f0fe54950be06bf4ee8443e07723f0
Artur Abdalimov <a.abdalimov.97@gmail.com>
2017-09-20 05:18:34 +0600

    PWD and DIR commands implemented

--- src/main/scala/server/ServerThread.scala
+++ src/main/scala/server/ServerThread.scala
@@ -1,6 +1,6 @@
 package server
 
-import java.io.{DataInputStream, DataOutputStream}
+import java.io.{DataInputStream, DataOutputStream, File}
 import java.net.{Socket, SocketException}
 
 /**
@@ -8,16 +8,40 @@ import java.net.{Socket, SocketException}
   */
 class ServerThread(val socket: Socket) extends Runnable {
 
+  private var currentWorkingDirectory = System.getProperty("user.home")
+
   override def run(): Unit = {
 
     try {
       while (socket.isConnected) {
         val received = new String(Stream.continually(socket.getInputStream.read).takeWhile(_ != '\n').map(_.toByte).toArray)
+        received.toUpperCase match {
+          case "DIR" => sendDirectories()
+          case "PWD" => sendPWD()
+          case _ => sendUnknown()
+        }
         println(received)
       }
     } catch {
       case s: SocketException => println("Socket closed")
     }
     println("End of session")
+  }
+
+
+  def sendDirectories(): Unit = {
+    val output = new File(currentWorkingDirectory).listFiles().map(_.getName).toList.mkString("\n")
+    socket.getOutputStream.write(output.getBytes, 0, output.length)
+    socket.getOutputStream.flush
+  }
+
+  def sendPWD(): Unit = {
+    val output = currentWorkingDirectory
+    socket.getOutputStream.write(output.getBytes, 0, output.length)
+    socket.getOutputStream.flush()
+  }
+
+  def sendUnknown(): Unit = {
+
   }
 }
